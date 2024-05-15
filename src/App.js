c801c7c9a071462d5677643f5786e3331efe8982
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-05-15 18:46:20 +0530

    program section is done with styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
 import Hero from "./components/Hero/Hero";
+import Programs from "./components/Programs/Programs";
 function App() {
   return <div className="App">
     <Hero/>
+    <Programs/>
   </div>;
 }
 
