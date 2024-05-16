e8e918d99f4a53b8293e2202f741ba446f1ad512
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-05-16 13:59:42 +0530

    Footer Component is done with proper styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 import Hero from "./components/Hero/Hero";
 import Join from "./components/Join/Join";
 import Plans from "./components/Plans/Plans";
@@ -6,14 +7,17 @@ import Programs from "./components/Programs/Programs";
 import Reasons from "./components/Reasons/Reasons";
 import Testimonials from "./components/Testimonials/Testimonials";
 function App() {
-  return <div className="App">
-    <Hero/>
-    <Programs/>
-    <Reasons/>
-    <Plans/>
-    <Testimonials/>
-    <Join/>
-  </div>;
+  return (
+    <div className="App">
+      <Hero />
+      <Programs />
+      <Reasons />
+      <Plans />
+      <Testimonials />
+      <Join />
+      <Footer/>
+    </div>
+  );
 }
 
 export default App;
